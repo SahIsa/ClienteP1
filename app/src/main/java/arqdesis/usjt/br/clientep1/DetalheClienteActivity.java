77a17b56cef9b2c78b616929adf25535070482c2
arqdsis <arqdsis>
2016-09-21 22:36:46 -0300

    Initial commit

---
+++ app/src/main/java/arqdesis/usjt/br/clientep1/DetalheClienteActivity.java
@@ -0,0 +1,19 @@
+package arqdesis.usjt.br.clientep1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetalheClienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe_cliente);
+        Intent intent = getIntent();
+        String nome = intent.getStringExtra (ListaClientesActivity.NOME);
+        TextView textView = (TextView)findViewById((R.id.nome_cliente);
+        textView.setText(nome);
+    }
+}
